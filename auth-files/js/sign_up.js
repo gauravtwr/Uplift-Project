0c628b9120f8eb8fa4fa60ec369747e964c7a2a1
gaurav <gauravtiwari20021995@gmail.com>
2020-12-27 23:51:02 +0530

    changes in relative path of the directories

--- auth-files/js/sign_up.js
+++ auth-files/js/sign_up.js
@@ -3,10 +3,10 @@ const passwordInput = document.getElementById("passwordInput");
 
 passwordToggle.addEventListener("click", function() {    
     if (passwordToggle.src.includes("show")){
-        passwordToggle.src = "/auth-files/media/images/icons/hide.svg";
+        passwordToggle.src = "../media/images/icons/hide.svg";
         passwordInput.type = "text";
     } else {
-        passwordToggle.src = "/auth-files/media/images/icons/show.svg";
+        passwordToggle.src = "../media/images/icons/show.svg";
         passwordInput.type = "password";
     }
 });
